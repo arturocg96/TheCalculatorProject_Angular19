23db8437ca1458d4203017d8aea8cbb23c4e5e08
ArturoCarrascoGonzál <acarrasco@leasba.com>
2025-03-11 13:38:18 +0100

    Styles

--- src/app/components/the-navbar/the-navbar.component.ts
+++ src/app/components/the-navbar/the-navbar.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-the-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './the-navbar.component.html',
   styleUrl: './the-navbar.component.css'
 })
 export class TheNavbarComponent {
 
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
